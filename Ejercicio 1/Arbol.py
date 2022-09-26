1f2819e39d68791d475238ba18f977eecdd1e516
JulianMolina <73005582+Juleeeaaannn@users.noreply.github.com>
2022-09-26 16:08:30 -0300

    Add files via upload

--- Ejercicio 1/Arbol.py
+++ Ejercicio 1/Arbol.py
@@ -1,5 +1,6 @@
 #Ejercicio Nº1: defina el objeto de datos árbol binario de búsqueda,
 # especifique e implemente todos los métodos vistas en teoría.
+from tkinter import N
 from Nodo import Nodo
 class Arbol:
     __raiz=None
@@ -46,18 +47,38 @@ class Arbol:
             else:
                 print("Elemento no encontrado!")
            
-    def Nivel(self):
-        pass
+    def Nivel(self,nodo,elemento,i=0):
+        if nodo!=None:
+            if nodo.getElemento() < elemento:
+                self.Nivel(nodo.getSigD(),elemento,i+1)
+            elif nodo.getElemento() > elemento:
+                self.Nivel(nodo.getSigI(),elemento,i+1)
+            else:
+                print(f"el nodo es de nivel: {i}")
+                return i
+        else:
+            print("nivel de elemento no encontrado")
+        
     def Hoja(self):
         pass
     def Hijo(self):
         pass
     def Padre(self):
         pass
-    def Camino(self):
-        pass
-    def Altura(self):
-        pass
+    def Camino(self, nodo,nodoInicio,nodoFinal):
+        if nodo!=None:
+            if nodo == nodoInicio:
+                print(f"{nodo.getElemento()}")
+            else:
+                if nodo!=nodoFinal:
+                    
+    def Altura(self,nodo:Nodo,i=0):
+        if nodo!=None:
+            aux1=self.Altura(nodo.getSigI(),i+1)
+            aux2=self.Altura(nodo.getSigD(),i+1)
+            return max(aux1,aux2)
+        else:
+            return i
     def InOrden(self,nodo):
         if nodo!=None:
             self.InOrden(nodo.getSigI())
@@ -82,10 +103,4 @@ if __name__ =='__main__':
     arbol.Insertar(arbol.Raiz(),"1")
     arbol.Insertar(arbol.Raiz(),"4")
     arbol.Insertar(arbol.Raiz(),"3")
-    print("Pre orden")
-    arbol.PreOrden(arbol.Raiz())
-    print("In orden")
-    arbol.InOrden(arbol.Raiz())
-    print("Post orden")
-    arbol.PostOrden(arbol.Raiz())
-    arbol.Busqueda(arbol.Raiz(),"5")
+    arbol.Nivel(arbol.Raiz(),"3")
